120b27f9cf917f273860f15caced379fc80be692
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-10-11 21:18:44 +0200

    Add schemas

---
+++ models/Journey.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const journeySchema = new mongoose.Schema({
+    seats: {
+        type: Number,
+        required: [true, 'Number of seats is required.'],
+    },
+    origin: {
+        type: String,
+        required: [true, 'Origin of the journey is required.'],
+    },
+    destination:{
+        type: String,
+        required: [true, 'Destination of the journey is required.'],
+    },
+    departureTime:{
+        type: Number,
+        required: [true, 'Departure time is required.'],
+    },
+    returnTime:{
+        type: Number,
+        required: [true, 'Departure time is required.'],
+    },
+    price:{
+        type:String,
+    },
+    date:{
+        type: Date,
+        required: [true, 'Date is required.'],
+        min: Date.now
+    },
+    creator:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+},
+{
+    toObject: { virtuals: true },
+  }
+)
+
+journeySchema.virtual("comments", {
+    ref: "Comment",
+    localField: "_id",
+    foreignField: "journey",
+    justOne: false,
+})
+journeySchema.virtual("score", {
+    ref: "Score",
+    localField: "_id",
+    foreignField: "journey",
+    justOne: false,
+})
+
+
+const Journey = mongoose.model('Journey', journeySchema);
+module.exports = Journey;
---
+++ models/User.model.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose')
+const bcryp = require('bcrypt')
+
+const ROLLUSER =['Driver', 'Buddy']
+const EMAIL_PATTERN = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+const PASSWORD_PATTERN = /^.{8,}$/i
+const SALT_ROUNDS = 10
+
+const UserSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, 'Name is requiered'],
+    minLength: [3, 'Name must contain at least 3 characters.'],
+    unique: [true, 'Username must be unique'],
+},
+email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    match: [EMAIL_PATTERN, 'Email must be valid.'],
+    unique: true,
+},
+password: {
+    type: String,
+    required: [true, 'Password is required.'],
+    match: [PASSWORD_PATTERN, 'Password must contain at least 8 characters.']
+},
+googleID: {
+    type: String,
+},
+image: {
+    type: String,
+    default: ''
+},
+roll: {
+    type: String,
+    enum: ROLLUSER
+}
+
+},
+{
+  toJSON: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      delete ret.__v;
+      delete ret._id;
+      delete ret.password;
+
+      return ret
+    }
+  }
+},
+{
+    toObject: { virtuals: true },
+  }
+)
+
+userSchema.pre('save', function(next){
+  if(this.isModified('password')){
+    bcryp.hash(this.password, SALT_ROUNDS)
+      .then(hash =>{
+        this.password = hash;
+        next()
+      } )
+  } else {
+    next()
+  }
+})
+
+UserSchema.methods.checkPassword = function(passwordToCompare) {
+  return bcrypt.compare(passwordToCompare, this.password);
+}
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
