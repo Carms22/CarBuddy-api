d9a9d6138e846a94f6dd182fb92ca62c6da1104d
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-10-17 13:07:05 +0200

    Add CORS

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Headers", "content-type");
+  res.set("Access-Control-Allow-Methods", "*");
+  res.set("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 
