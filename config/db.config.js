1bf8e80dc0caf86690db4fc8e7d358c940e2009b
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-11-12 12:28:05 +0100

    test32

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DB_NAME = 'CarBuddy';
+const DB_NAME = 'Cluster0';
 const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
 const DB_URI = `${URI}`;
 
